f2ebe4a846169624906c3f7c9dac57e473291525
saranlivingmobile <saran@livingmobile.me>
2021-06-18 11:56:42 +0700

    Set install

--- backend-rest/src/app.module.ts
+++ backend-rest/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'secret',
+      database: 'AssignmentAPI',
+      autoLoadModels: true,
+      synchronize: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
